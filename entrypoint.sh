c5fb65fd0fa34d578b74b365050cd4e966c08c2e
Graham Turner <grahamturnernew@hotmail.com>
2018-12-02 13:05:39 +0000

    Working under a git repo
    Signed-off-by: Graham Turner <grahamturnernew@hotmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,13 @@ set +e
 set +x
 set +u
 
+sudo su
+
 if [ ! -z "$(cat /etc/os-release | grep mint)" ] ;
 then
-    echo "found Debian"
+    echo "found mint"
     apt update
     apt install -y git git-core python python-dev python-pip
     pip install ansible
-    git clone https://github.com/backenddeveloper/configuration
-    ansible-playbook configuration/packages.yml
+    ansible-playbook packages.yml
 fi
