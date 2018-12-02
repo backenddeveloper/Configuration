100cb9f8f9c6f55a7baf00e62ba2f161c2ade75f
Graham Turner <grahamturnernew@hotmail.com>
2018-12-02 12:38:43 +0000

    Adding packages
    Signed-off-by: Graham Turner <grahamturnernew@hotmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ then
     apt update
     apt install -y git git-core python python-dev python-pip
     pip install ansible
+    git clone https://github.com/backenddeveloper/configuration
+    ansible-playbook configuration/packages.yml
 fi
 if [ ! -z "$(cat /etc/os-release | grep ID_LIKE | grep centos)" ] ;
 then
