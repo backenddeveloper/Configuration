80e90f8b3b6b1817340bbaec4071c0f3a1df16b2
Graham Turner <grahamturnernew@hotmail.com>
2018-12-02 13:22:48 +0000

    Adding sudoers.d
    Signed-off-by: Graham Turner <grahamturnernew@hotmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,17 @@ set +e
 set +x
 set +u
 
-sudo su
+username="$(whoami)"
+
+sudo -u root bash <<HEREDOC
 
 if [ ! -z "$(cat /etc/os-release | grep mint)" ] ;
 then
     echo "found mint"
     apt update
     apt install -y git git-core python python-dev python-pip
-    pip install ansible
-    ansible-playbook packages.yml
+    pip install wheel ansible
+    ansible-playbook -e username=${username} packages.yml
 fi
+
+HEREDOC
